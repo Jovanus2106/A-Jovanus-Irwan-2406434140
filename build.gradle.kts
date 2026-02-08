122ec533369c093e254741b622b6dee091213288
Jovanus Irwan <jovanusirwan@gmail.com>
2026-02-09 00:37:18 +0700

    build gradle.kts push

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,11 @@ plugins {
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
-description = "Eshops"
-
+description = "eshop"
+val seleniumJavaVersion= "4.14.1"
+val seleniumJupiterVersion="5.0.1"
+val webdrivermanagerVersion= "5.6.3"
+val junitJupiterVersion= "5.9.1"
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
@@ -31,11 +34,31 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    //    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
+    // saya tidak menggunakan ini dikarenakan punya JUnit punya saya di-override manual dan
+    // selenium jupiter saya bentrok dengan JUnit Spring Boot saya.
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.github.bonigarcia:webdrivermanager:${webdrivermanagerVersion}")
+}
+tasks.register<Test>("unitTest"){
+    description= "Runs unit tests."
+    group= "verification"
+    filter{
+        excludeTestsMatching("*FunctionalTest")
+    }
+}
+tasks.register<Test>("functionalTest"){
+    description= "Runs functional tests"
+    group= "verification"
+    filter{
+        includeTestsMatching("*FunctionalTest")
+    }
+}
 tasks.withType<Test> {
     useJUnitPlatform()
 }
