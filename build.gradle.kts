b152bffcb94ca09233976deb9fad28e47b256c1a
Jovanus Irwan <jovanusirwan@gmail.com>
2026-02-22 21:35:12 +0700

    mencoba perbaikan sonarcloud part1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.sonarqube") version "4.4.1.3373"
+    id("org.sonarqube") version "5.0.0.4638"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -74,6 +74,10 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
 
 sonar {
@@ -81,11 +85,11 @@ sonar {
     properties {
 
         property("sonar.projectKey", "Jovanus2106_A-Jovanus-Irwan-2406434140")
-
         property("sonar.organization", "jovanus2106")
 
         property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+    }
 
     }
 
-}
