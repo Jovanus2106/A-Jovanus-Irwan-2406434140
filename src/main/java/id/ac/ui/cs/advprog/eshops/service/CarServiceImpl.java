6dd28316ecc526b25776cd4be2a3e22095133101
Jovanus Irwan <jovanusirwan@gmail.com>
2026-03-01 23:15:59 +0700

    html templates dan perubahan service sedikit

--- src/main/java/id/ac/ui/cs/advprog/eshops/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshops/service/CarServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+@Service
 public class CarServiceImpl implements CarService {
     @Autowired
     private CarRepository carRepository;
