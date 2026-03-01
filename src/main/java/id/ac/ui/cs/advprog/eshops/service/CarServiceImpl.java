b520b09985bbaa22717be67489fa5f7dd9d560e5
Jovanus Irwan <jovanusirwan@gmail.com>
2026-03-01 20:26:05 +0700

    car service dan carserviceImpl

---
+++ src/main/java/id/ac/ui/cs/advprog/eshops/service/CarServiceImpl.java
@@ -0,0 +1,43 @@
+package id.ac.ui.cs.advprog.eshops.service;
+
+import id.ac.ui.cs.advprog.eshops.model.Car;
+import id.ac.ui.cs.advprog.eshops.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+public class CarServiceImpl implements CarService {
+    @Autowired
+    private CarRepository carRepository;
+
+    @Override
+    public Car create(Car car) {
+        carRepository.create(car);
+        return car;
+    }
+
+    @Override
+    public void update(String carId, Car car) {
+        carRepository.update(carId, car);
+    }
+
+    @Override
+    public void deleteCarbyId(String carId) {
+        carRepository.delete(carId);
+    }
+    @Override
+    public List<Car> findAll(){
+        Iterator<Car>carIterator= carRepository.findAll();
+        List<Car> allcar= new ArrayList<>();
+        carIterator.forEachRemaining(allcar::add);
+        return allcar;
+    }
+
+    @Override
+    public Car findById(String carId) {
+        Car car = carRepository.findById(carId);
+        return car;
+    }
+
+}
